9ca0cce62defe3d4c5623ef7be78f6b0981966f2
永谷 黎 <nagayarei@team-lab.com>
2018-08-22 12:21:26 +0900

    [add]router設定

--- app/config/router.php
+++ app/config/router.php
@@ -2,5 +2,23 @@
 
 $router = $di->getRouter();
 
+// Login　Getメソッドだったときのみ有効
+$router->addGet(
+    '/login',
+    'Session::index'
+);
+
+// Login　Postメソッドだったときのみ有効
+$router->addPost(
+    '/login',
+    'Session::start'
+);
+
+// Logout Getメソッドだったときのみ有効
+$router->addGet(
+    '/logout',
+    'Session::end'
+);
+
 // $_GET["_url"]からURLを取得
 $router->handle();
