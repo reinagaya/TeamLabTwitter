5b1198d2a55cdddf9685ec3a7e59018e2aa629fe
永谷 黎 <nagayarei@team-lab.com>
2018-08-23 16:08:13 +0900

    [add]フォローの登録機能

--- app/plugins/SecurityPlugin.php
+++ app/plugins/SecurityPlugin.php
@@ -52,7 +52,7 @@ class SecurityPlugin extends Plugin
                 'users'      => ['index'],
                 'session'    => ['end'],
                 'tweet'      => ['index','posttweet'],
-                'follow'     => ['index']
+                'follow'     => ['index','newfollow','setfollow']
             ];
             foreach ($privateResources as $resource => $actions) {
                 $acl->addResource(new Resource($resource), $actions);
@@ -72,6 +72,7 @@ class SecurityPlugin extends Plugin
                 'index'      => ['index'],
                 'session'    => ['end'],
                 'tweet'      => ['index','posttweet'],
+                'follow'     => ['newfollow','setfollow']
             ];
 
             // 管理者にprivateResourceのすべてのアクセス権を付与
