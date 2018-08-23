b848feee8a7dc5acb5a90c108a147c37eca6a704
永谷 黎 <nagayarei@team-lab.com>
2018-08-23 12:52:56 +0900

    [fix]新規登録後のログイン画面からも再度新規登録できるようにした

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -61,10 +61,7 @@ class UsersController extends \Phalcon\Mvc\Controller
         // Log
         $this->flash->success('user was created successfully');
 
-        // sessionのindexに飛ばす
-        $this->dispatcher->forward([
-            'controller' => 'session',
-            'action' => 'index'
-        ]);
+        // ログイン画面に飛ぶ
+        $this->response->redirect("login");
     }
 }
