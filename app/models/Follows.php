35ad34a2ad6a879d6f29096acf5f788a5bc012fd
永谷 黎 <nagayarei@team-lab.com>
2018-08-23 16:35:47 +0900

    [update]フォローの重複を避ける

--- app/models/Follows.php
+++ app/models/Follows.php
@@ -21,6 +21,23 @@ class Follows extends \Phalcon\Mvc\Model
      */
     public $followee_id;
 
+    // セーブ前のバリデーション
+    public function beforeSave()
+    {
+        // 完全一致するデータを探す
+        $val_follower = "follower_id = '" . $this->follower_id . "'";
+        $val_followee = "followee_id = '" . $this->followee_id . "'";
+        $exist = $this->find($val_follower . " and " . $val_followee);
+
+        // 検索数が０でなかったらエラー
+        if (count($exist) !== 0) {
+            echo "you already follow up this user<br>";
+            return false;
+        }
+
+        return true;
+    }
+
     /**
      * Initialize method for model.
      */
